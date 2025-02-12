b9b7d2b3501a5705848353d3c4faccc3106c2aec
flaviusp23 <flavius.paltin@yahoo.com>
2025-02-13 00:43:10 +0200

    summary reservation page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/flaviusp23/bookings/internal/config"
 	"github.com/flaviusp23/bookings/internal/handlers"
+	"github.com/flaviusp23/bookings/internal/models"
 	"github.com/flaviusp23/bookings/internal/render"
 
 	"github.com/alexedwards/scs/v2"
@@ -19,6 +21,8 @@ var app config.AppConfig
 var session *scs.SessionManager
 
 func main() {
+	//what am i going to put in the session
+	gob.Register(models.Reservation{})
 
 	//change this to true when in production
 	app.InProduction = false
