e6e94eda3c78a8a2c0c4f7477ebf4898ae4ef758
flaviusp23 <flavius.paltin@yahoo.com>
2025-02-13 16:56:34 +0200

    session testing

---
+++ internal/render/render_test.go
@@ -0,0 +1,38 @@
+package render
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/flaviusp23/bookings/internal/models"
+)
+
+func TestAddDefaultData(t *testing.T) {
+	var td models.TemplateData
+
+	r, err := getSession()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	session.Put(r.Context(), "flash", "123")
+
+	result := AddDefaultData(&td, r)
+	if result.Flash != "123" {
+		t.Error("flash value of 123 not found in session")
+	}
+
+}
+
+// This function creates a fake HTTP request and attaches a session to it. This is needed because in tests, you don’t have a real web browser sending requests, so you must simulate one.
+func getSession() (*http.Request, error) {
+	r, err := http.NewRequest("GET", "/some-url", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := r.Context()
+	ctx, _ = session.Load(ctx, r.Header.Get("X-Session"))
+	r = r.WithContext(ctx)
+	return r, nil
+}
