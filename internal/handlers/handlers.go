3281e1700ec727dd54a38e769a7fbed39724fec7
flaviusp23 <flavius.paltin@yahoo.com>
2025-02-14 19:31:22 +0200

    insert reservations

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/flaviusp23/bookings/internal/config"
 	"github.com/flaviusp23/bookings/internal/driver"
@@ -65,12 +67,31 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		helpers.ServerError(w, err)
 		return
 	}
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
 
+	layout := "2006-01-02"
+	startDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Email:     r.Form.Get("email"),
 		Phone:     r.Form.Get("phone"),
+		StartDate: startDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
 	}
 
 	form := forms.New(r.PostForm)
@@ -88,6 +109,10 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 			Data: data,
 		})
 		return
+	}
+	err = m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
 	}
 	m.App.Session.Put(r.Context(), "reservation", reservation)
 	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,8 @@
 package repository
 
+import "github.com/flaviusp23/bookings/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+	InsertReservation(res models.Reservation) error
 }
