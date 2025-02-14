15a5d52d2d258b5147d7c0a2c22f77c6e86c9036
flaviusp23 <flavius.paltin@yahoo.com>
2025-02-14 17:10:29 +0200

    driver and connecting to DB

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/flaviusp23/bookings/internal/config"
+	"github.com/flaviusp23/bookings/internal/driver"
 	"github.com/flaviusp23/bookings/internal/handlers"
 	"github.com/flaviusp23/bookings/internal/helpers"
 	"github.com/flaviusp23/bookings/internal/models"
@@ -25,11 +26,12 @@ var infoLog *log.Logger
 var errorLog *log.Logger
 
 func main() {
-	err := run()
+	db, err := run()
 
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.SQL.Close()
 
 	fmt.Printf("Staring application on port %s", portNumber)
 
@@ -44,7 +46,7 @@ func main() {
 	}
 }
 
-func run() error {
+func run() (*driver.DB, error) {
 	//what am i going to put in the session
 	gob.Register(models.Reservation{})
 
@@ -64,16 +66,23 @@ func run() error {
 
 	app.Session = session
 
+	//connect to db
+	log.Println("Connecting to database...")
+	db, err := driver.ConnectSQL("host=localhost port=5432 dbname=bookings-project user=postgres password=1234")
+	if err != nil {
+		log.Fatal("Cannot connect to database! Dying...")
+	}
+	log.Println("Connected to database!")
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
-		return err
+		return nil, err
 	}
 	app.TemplateCache = tc
 	app.UseCache = false
-	repo := handlers.NewRepo(&app)
+	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 	helpers.NewHelpers(&app)
-	return nil
+	return db, nil
 }
