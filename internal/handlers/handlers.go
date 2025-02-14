d744b7f2115f7c39df7ecd7b1cd163f87b7df986
flaviusp23 <flavius.paltin@yahoo.com>
2025-02-14 22:30:42 +0200

    connecting search availability with make reservation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -15,6 +15,7 @@ import (
 	"github.com/flaviusp23/bookings/internal/render"
 	"github.com/flaviusp23/bookings/internal/repository"
 	"github.com/flaviusp23/bookings/internal/repository/dbrepo"
+	"github.com/go-chi/chi/v5"
 )
 
 // Repo the repository used by the handlers
@@ -167,9 +168,9 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 		helpers.ServerError(w, err)
 		return
 	}
-	for _, i := range rooms {
-		m.App.InfoLog.Println("ROOM:", i.ID, i.RoomName)
-	}
+	// for _, i := range rooms {
+	// 	m.App.InfoLog.Println("ROOM:", i.ID, i.RoomName)
+	// }
 	if len(rooms) == 0 {
 		m.App.Session.Put(r.Context(), "error", "No availability")
 		http.Redirect(w, r, "/search-availability", http.StatusSeeOther)
@@ -232,4 +233,22 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 	render.Template(w, r, "reservation-summary.page.tmpl", &models.TemplateData{
 		Data: data,
 	})
+}
+
+func (m *Repository) ChooseRoom(w http.ResponseWriter, r *http.Request) {
+	roomID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+	res, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		helpers.ServerError(w, err)
+		return
+	}
+	res.RoomID = roomID
+	m.App.Session.Put(r.Context(), "reservation", res)
+
+	http.Redirect(w, r, "/make-reservation", http.StatusSeeOther)
+
 }
