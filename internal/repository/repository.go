1b3cd83a07da86a56a4833ebd4c1885f8a02b5df
flaviusp23 <flavius.paltin@yahoo.com>
2025-02-15 00:44:02 +0200

    connecting rooms page to make reservation

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -10,6 +10,7 @@ type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(res models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 	GetRoomByID(id int) (models.Room, error)
 }
