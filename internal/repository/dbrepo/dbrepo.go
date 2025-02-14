e0e37513c64ec66261dbb24ee9584a23464fb3e1
flaviusp23 <flavius.paltin@yahoo.com>
2025-02-14 17:10:29 +0200

    driver and connecting to DB

---
+++ internal/repository/dbrepo/dbrepo.go
@@ -0,0 +1,21 @@
+package dbrepo
+
+import (
+	"database/sql"
+
+	"github.com/flaviusp23/bookings/internal/config"
+	"github.com/flaviusp23/bookings/internal/repository"
+)
+
+// here we can add many more DB connects like MySQL, MariaDB etc
+type postgresDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
+func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+	return &postgresDBRepo{
+		App: a,
+		DB:  conn,
+	}
+}
