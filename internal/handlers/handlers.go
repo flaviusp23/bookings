d2608ce11abb0c0444b43c65cccef7d0bd304c88
flaviusp23 <flavius.paltin@yahoo.com>
2025-02-16 21:01:34 +0200

    admin templates

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -489,3 +489,41 @@ func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-dashboard.page.tmpl", &models.TemplateData{})
 }
+
+// AdminAllReservations shows all reservations inu admin tool
+func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request) {
+	// reservations, err := m.DB.AllReservations()
+	// if err != nil {
+	// 	helpers.ServerError(w, err)
+	// 	return
+	// }
+
+	// data := make(map[string]interface{})
+	// data["reservations"] = reservations
+
+	render.Template(w, r, "admin-all-reservations.page.tmpl", &models.TemplateData{
+		Data: nil,
+	})
+}
+
+// AdminNewReservations shows all new reservations in admin tool
+func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request) {
+	// reservations, err := m.DB.AllNewReservations()
+	// if err != nil {
+	// 	helpers.ServerError(w, err)
+	// 	return
+	// }
+
+	// data := make(map[string]interface{})
+	// data["reservations"] = reservations
+	render.Template(w, r, "admin-new-reservations.page.tmpl", &models.TemplateData{
+		Data: nil,
+	})
+}
+
+// AdminReservationsCalendar shows all new reservations in admin tool
+func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{
+		Data: nil,
+	})
+}
