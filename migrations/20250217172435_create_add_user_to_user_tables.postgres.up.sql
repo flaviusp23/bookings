e0321c003d51056da24aa008e4e974b04aba2e2f
flaviusp23 <flavius.paltin@yahoo.com>
2025-02-17 19:25:51 +0200

    seed admin user

---
+++ migrations/20250217172435_create_add_user_to_user_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+(E'Flavius',E'Paltin',E'admin@admin.com',E'$2a$12$gSARI7iVKc9n.ZvpUb7iY.PCM934AsfWa57QpyQ8XxmkrW.B3y2l6',3,E'2020-12-04 00:00:00',E'2020-12-04 00:00:00');
